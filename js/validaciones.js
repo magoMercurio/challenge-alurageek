bc0308dfe14cea291c3a774fc4eb468391dcd16a
mago_d_Mercurio <davila.feria@gmail.com>
2023-03-30 11:46:30 -0500

    add js validador
    add form validador

---
+++ js/validaciones.js
@@ -0,0 +1,54 @@
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+
+    
+    if (input.validity.valid) {
+        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = ""
+    } else {
+        input.parentElement.classList.add("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input)
+
+    }
+}
+
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+]
+
+const mensajesDeError = {
+    nombre: {
+        valueMissing: "Este campo no puede estar vacio"
+    },
+    email: {
+        valueMissing: "Este campo no puede estar vacio",
+        typeMismatch: "El correo no es valido"
+    },
+    asunto: {
+        valueMissing: "Este campo no puede estar vacio",
+        typeMismatch: "agrega un titulo al Asunto"
+    },
+    mensaje: {
+        valueMissing: "Este campo no puede estar vacio",
+        typeMismatch: "Escribe algo que quieras decirme."
+    },
+    password: {
+        valueMissing: "Este campo no puede estar vacio",
+        patternMismatch: "Al menos 6 caracteres maximo 12, debe contener una letra minuscula, una letra mayuscula, un numero y no puede contener caracteres especiales" 
+    }
+}
+
+function mostrarMensajeDeError(tipoDeInput, input) {
+    let mensaje = "";
+    tipoDeErrores.forEach((error) => {
+        if (input.validity[error]) {
+            mensaje = mensajesDeError[tipoDeInput][error];
+        }
+    });
+
+    return mensaje
+}
+
